263e0619a6f8c6b1e4ad5abbf24f12821ad905b7
freezeava <aashishava301@gmail.com>
2024-08-28 22:01:31 +0530

    learning string methods in js

---
+++ level2/arrays.js
@@ -0,0 +1,12 @@
+// let marks=[22,23,2,23,44,55,555];
+// for(let i=1;i<marks.length;i++){
+//     console.log(marks[i]+" ");
+// }
+
+let marks=[22,23,2,23,44,55,555];
+for(let i=0;i<marks.length;i++){
+    for(let j=0;j<marks.length;j++){
+
+    console.log(marks[i][j]+" ");
+}
+};
---
+++ level2/stringmethod.js
@@ -0,0 +1,11 @@
+let fname="  Aashish vidyarthi   ";
+console.log(fname.trim().toUpperCase());
+console.log(fname.indexOf("h"));
+
+let lname="shyamlala"
+let splicem= lname.slice(3,8);//amlal
+console.log(splicem);
+
+let girl="Shruti";
+console.log(girl.replace("Sh","ru"));
+console.log(girl.repeat(3));
